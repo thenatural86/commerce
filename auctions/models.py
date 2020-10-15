c5c7a4a88f2d6043c70287e67c53345abb58e91a
thenatural86 <philip.smalls86@gmail.com>
2020-10-15 18:43:27 -0400

    initial models

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,21 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listings(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    bid = models.IntegerField()
+    category = models.CharField(max_length=64)
+
+
+class Bid(models.Model):
+    user = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
+    bid = models.IntegerField()
+
+
+class Comment(models.Model):
+    user = models.CharField(max_length=64)
+    comment = models.TextField()
